579844d4ce7fd196b8938f90c0f8b4eb3eb0ad81
Dave Yoon <daveyoon64@gmail.com>
2021-03-31 15:28:16 -0700

    Exmperimention with lifecycle method to get top stories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
 import './App.css';
-import {testGet} from './HNservice'
+import {getTopStories, getStory} from './HNservice';
 
 class App extends Component {
+  state = {
+    title: '',
+    url: '',
+    score: 0,
+    author: '',
+    comments: 0
+  }
   
-  componentDidMount() {
-    testGet()
-      .then(item => console.log(item))
-    console.log()
+  // get story numbers from https://hacker-news.firebaseio.com/v0/topstories.json
+    // this still be the state we use for our pagination
+  // to display story...
+    // get a number from story numbers
+      //and create new_story like we did below
+
+  async componentDidMount() {
+    // testing
+    const top_stories = await getTopStories(); 
+    console.log(await top_stories);
+    console.log(await getStory(top_stories[0]));
+
+    const url = 'https://hacker-news.firebaseio.com/v0/item/26638145.json';
+    const response = await fetch(url);
+    const data = await response.json();
+    const new_story = {
+      title: data.title,
+      url: data.url,
+      score: data.score,
+      author: data.by,
+      comments: data.descendants
+    }
+    this.setState({...new_story});
   }
 
   render() {
@@ -16,10 +42,24 @@ class App extends Component {
         <header className="App-header">
           Jack Reactor: A Hacker News Clone
         </header>
-        <div className="Feed-list">
-          <p>Stories go here man</p>
-          <p>Intel is Dope Again</p>
-          <p>Apple changes name to Banana</p>
+        <div className="Feed-list-container">
+          <ol className="Feed-list">
+            <li>this is story test</li>
+            <li>this is story test</li>
+            <li>this is story test</li>
+            <li className="Story">
+              <a href={this.state.url}>{this.state.title}dkjgasdkljgahsdklgjashdlkgahsdklgashdklgjahsdlkga</a>
+              <div className="Story-options">
+                {this.state.score} points by {this.state.author}ldfkgjsdfl;gsjfgl;sj TIMESTAMP | {this.state.comments} comments
+              </div>
+            </li>
+            <li className="Story">
+              <a href={this.state.url}>{this.state.title}</a>
+              <div className="Story-options">
+                {this.state.score} points by {this.state.author} TIMESTAMP | {this.state.comments} comments
+              </div>
+            </li>
+          </ol>
         </div>
         <div className="Footer">
           <p>Made with love Muwwwahahhhhh</p>
--- src/HNservice.js
+++ src/HNservice.js
@@ -1,7 +1,20 @@
 
-let test = ' https://hacker-news.firebaseio.com/v0/item/26638145.json';
+let test = 'https://hacker-news.firebaseio.com/v0/item/26638145.json';
 
-export const testGet = () => {
-  return fetch(test)
-    .then(response => response.json())
+export const getTopStories = async() => {
+  // Gets the top 500 stories as numbers from Hacker News' API
+  // data looks like: [3434523, 3453463, ...]
+  const url = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+export const getStory = async(number) => {
+  // asynchronous function to grab story from Hacker News
+  const url = 'https://hacker-news.firebaseio.com/v0/item/';
+  const storyUrl = url + number + '.json';
+  const response = await fetch(storyUrl);
+  const story = await response.json();
+  return story;
 }
