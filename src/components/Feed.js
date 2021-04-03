cba84d5cf58cefbeed5060c7dfa89c4f89c751ea
Dave Yoon <daveyoon64@gmail.com>
2021-04-02 23:50:12 -0700

    Adding unique keys for stories

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,5 @@
 import {React} from 'react';
+import {generateId} from '../util';
 
 export const Feed = (props) => {
   return(
@@ -6,7 +7,7 @@ export const Feed = (props) => {
       <ol className="Feed-list" start={props.index + 1}>
         {props.stories.length 
           ? props.stories.map(story => 
-            <li className="Story">
+            <li className="Story" key={generateId()}>
               <a href={story.url}>{story.title}</a>
               <div className="Story-options">
                 {story.score} points by {story.author} TIMESTAMP | {story.comments} comments
